c32f83bc2fbd4e3a09a9b5ebd8bcfe85ee1c0a6a
hmurtaza720 <hmurtaza510@gmail.com>
2025-09-04 14:40:00 +0500

    task completed

---
+++ HelloCSharp/HelloCSharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variable declaration and initialization
+            int age = 23;
+            string name = "Murtaza";
+            double cgpa = 3.17;
+            bool Intern = true; 
+
+            Console.WriteLine("My Name: " + name);
+            Console.WriteLine("MY Age: " + age);
+            Console.WriteLine("My CGPA: " + cgpa);
+            Console.WriteLine("Intern at TPL : " + Intern);
+            Console.ReadLine();
+            }
+    }
+}
---
+++ Temperature Converter/Temperature Converter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Temperature_Converter
+{
+   class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter temperature in Celsius:");
+            double cel = Convert.ToDouble(Console.ReadLine());
+            double f = (cel * 9 / 5) + 32;
+            Console.WriteLine("C : " + cel + " F : " + f);
+
+            Console.WriteLine("Enter temperature in Fahrenheit:");
+            double fa = Convert.ToDouble(Console.ReadLine());
+            double c = (fa - 32) * 5 / 9;
+            Console.WriteLine("F : " + fa + " C : " + c);
+
+            Console.ReadLine();
+
+        }
+    }
+}
