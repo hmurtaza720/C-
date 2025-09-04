704926a267b3eb4614d7d31ccfbb790228d960e1
hmurtaza720 <hmurtaza510@gmail.com>
2025-09-04 16:23:09 +0500

    variable&constant revision

---
+++ Variables & Constants/Variables & Constants/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Variables_Constants
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = 3.1416;
+
+            int radius = 5;
+            var diameter = radius *2;
+            double area = pi * radius * radius;
+
+            Console.WriteLine("Radius: "+ radius);
+            Console.WriteLine("Diameter: "+ diameter);
+            Console.WriteLine("Area: "+ area);
+            Console.ReadLine();
+        }
+    }
+}
