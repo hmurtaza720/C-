3fec330412d53d27a79dc41fdfd43ce829e517ea
i_Murtaza <i_Murtaza@tplinsurance.com>
2025-10-01 16:11:58 +0500

    Exception Handling

---
+++ Exception Handling/Exception Handling/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class NegativeMarksException : Exception
+{
+    public NegativeMarksException(string message) : base(message) { }
+}
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Enter student marks: ");
+            int marks = Convert.ToInt32(Console.ReadLine());
+
+            if (marks < 0)
+                throw new NegativeMarksException("Marks cannot be negative!");
+
+            Console.WriteLine($"Marks entered: {marks}");
+        }
+        catch (NegativeMarksException ex)
+        {
+            Console.WriteLine(" Custom Error: " + ex.Message);
+        }
+    }
+}
